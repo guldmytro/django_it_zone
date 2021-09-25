d328146d02bf1b077ee0b59a8ce6cbde525b1620
Dmytro Gul <guldmytro@gmail.com>
2021-09-25 13:19:18 +0300

    Added Meta Classes for Attributes and Kits

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category, Product
+from .models import Category, Product, Kit, Attribute
 
 
 @admin.register(Category)
@@ -8,9 +8,20 @@ class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
 
 
+class KitInline(admin.StackedInline):
+    model = Kit
+
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug', 'price', 'available', 'created', 'updated']
     list_filter = ['available', 'created', 'updated']
     list_editable = ['price', 'available']
+    prepopulated_fields = {'slug': ('name',)}
+    inlines = [KitInline]
+
+
+@admin.register(Attribute)
+class AttributeAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
     prepopulated_fields = {'slug': ('name',)}
--- catalog/models.py
+++ catalog/models.py
@@ -20,10 +20,24 @@ class Category(models.Model):
         return self.name
 
 
+class Attribute(models.Model):
+    name = models.CharField(max_length=100, verbose_name='название')
+    slug = models.SlugField(max_length=100, unique=True, verbose_name='слаг')
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'атрибут'
+        verbose_name_plural = 'атрибуты'
+
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
     category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE, verbose_name='категория')
     name = models.CharField(max_length=200, db_index=True, verbose_name='название')
     slug = models.SlugField(max_length=200, db_index=True, verbose_name='слаг')
+    attributes = models.ManyToManyField(Attribute, through='Kit', through_fields=('product', 'attribute'))
     image = models.ImageField(upload_to='images/%Y/%m/%d', blank=True, verbose_name='изображение')
     description = models.TextField(blank=True, verbose_name='описание')
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='цена',
@@ -50,3 +64,16 @@ class Product(models.Model):
             errors['price_sale'] = ValidationError(message='Цена со скидкой должна быть ниже обычной цены')
         if errors:
             raise ValidationError(errors)
+
+
+class Kit(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name='товар')
+    attribute = models.ForeignKey(Attribute, on_delete=models.CASCADE, verbose_name='атрибут')
+    value = models.CharField(max_length=100, verbose_name='значение')
+
+    class Meta:
+        verbose_name = 'атрибут'
+        verbose_name_plural = 'атрибуты'
+
+    def __str__(self):
+        return self.attribute.name
