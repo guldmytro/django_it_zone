e161f1225b6172a087f0e08a07089505d571510c
Dmytro Gul <guldmytro@gmail.com>
2021-09-25 10:26:40 +0300

    Added Postgresql connection

--- shop/settings.py
+++ shop/settings.py
@@ -73,8 +73,11 @@ WSGI_APPLICATION = 'shop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'itzone_db',
+        'USER': 'dbadmin',
+        'PASSWORD': 'abc123!',
+        'HOST': 'localhost'
     }
 }
 
