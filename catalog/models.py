33c8a0996c2bcb07c93c3e83ea7cd14ea92f8bc4
Dmytro Gul <guldmytro@gmail.com>
2021-09-25 11:44:22 +0300

    Updated Models

--- catalog/models.py
+++ catalog/models.py
@@ -6,6 +6,10 @@ from django.core import validators
 class Category(models.Model):
     name = models.CharField(max_length=200, db_index=True, verbose_name='название')
     slug = models.SlugField(max_length=200, unique=True, verbose_name='слаг')
+    parent_category = models.ForeignKey('self', blank=True, verbose_name='родительска категория',
+                                        on_delete=models.CASCADE)
+    description = models.TextField(blank=True, verbose_name='описание')
+    image = models.ImageField(upload_to='images/%Y/%m/%d', blank=True, verbose_name='изображение')
 
     class Meta:
         ordering = ('name',)
@@ -20,7 +24,7 @@ class Product(models.Model):
     category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE, verbose_name='категория')
     name = models.CharField(max_length=200, db_index=True, verbose_name='название')
     slug = models.SlugField(max_length=200, db_index=True, verbose_name='слаг')
-    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True, verbose_name='изображение')
+    image = models.ImageField(upload_to='images/%Y/%m/%d', blank=True, verbose_name='изображение')
     description = models.TextField(blank=True, verbose_name='описание')
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='цена',
                                 validators=[validators.MinValueValidator(0, 'Цена не может быть ниже нуля')])
@@ -44,6 +48,5 @@ class Product(models.Model):
         errors = {}
         if self.price_sale and self.price_sale > self.price:
             errors['price_sale'] = ValidationError(message='Цена со скидкой должна быть ниже обычной цены')
-
         if errors:
             raise ValidationError(errors)
